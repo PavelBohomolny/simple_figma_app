c22221660c905bf9bb82798202069bb404f6d289
NexusUA <pbogomolny93@gmal.com>
2022-08-04 13:18:02 +0200

    Initial Commit

---
+++ SimpleFigmaApp/SimpleFigmaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFigmaAppApp.swift
+//  SimpleFigmaApp
+//
+//  Created by Pavel Bohomolnyi on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFigmaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
